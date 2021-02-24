929bfe52f7279227875a1788d275edd254a17c76
lu-castro <luciacastro.23.5@gmail.com>
2021-02-23 21:35:49 -0300

    thrid change

--- venv/mysite/app_/views.py
+++ venv/mysite/app_/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context 
 
-from django.views.generic import TemplateView
-
 def Index(request):
   return render(request, 'index.html')
