95e9c7b46dc04e1d934226d0175dcb9d4981ffd3
lu-castro <luciacastro.23.5@gmail.com>
2021-02-24 21:14:06 -0300

    changes

--- Oeste_b/mysite/app_/views.py
+++ Oeste_b/mysite/app_/views.py
@@ -1,6 +1,1 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template, Context 
-
-def Index(request):
-  return render(request, 'index.html')
+from django.shortcuts import renders
--- Oeste_b/mysite/mysite/urls.py
+++ Oeste_b/mysite/mysite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app_.views import Index
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Index),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
