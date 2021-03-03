23f5e4e3815ea836bdefde81702d59df9eda6a54
lu-castro <luciacastro.23.5@gmail.com>
2021-03-03 13:51:11 -0300

    models

--- Oeste_b/mysite/app_/models.py
+++ Oeste_b/mysite/app_/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Sabrocitos(models.Model):
+  nombre=models.CharField(max_length=30)
+  gramos=models.CharField(max_length=8)
+  precio=models.FloatField()
+
+class Raza(models.Model):
+  nombre=models.CharField(max_length=30)
+  gramos=models.CharField(max_length=8)
+  precio=models.FloatField()
+
+class dog_Selection(models.Model):
+  nombre=models.CharField(max_length=30)
+  gramos=models.CharField(max_length=8)
+  precio=models.FloatField()
+
+class Pedigree(models.Model):
+  nombre=models.CharField(max_length=30)
+  gramos=models.CharField(max_length=8)
+  precio=models.FloatField()
+
+class Eukanuba(models.Model):
+  nombre=models.CharField(max_length=30)
+  gramos=models.CharField(max_length=8)
+  precio=models.FloatField()
