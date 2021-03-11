ea0f06daed12db614c61d3295652bd2bdd682dea
lu-castro <luciacastro.23.5@gmail.com>
2021-03-11 12:45:44 -0300

    admin

--- Oeste_b/mysite/app_/admin.py
+++ Oeste_b/mysite/app_/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from app_.models import Cliente, Pedido, Producto
+
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin): 
+  list_display=("nombre", "email", "telefono")
+  search_fields=("nombre", "email")
+
+class PedidosAdmin(admin.ModelAdmin):
+  search_fields=("cliente", "pedido", "entrega")
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Pedido, PedidosAdmin)
+admin.site.register(Producto)
